fb51a467a056ca8ae65238bf06449efff0a4f622
Shawn Rice <Shawn.CS@protonmail.com>
2021-09-16 23:47:17 -0400

    App complete for now

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 
-import React, { Fragment } from 'react';
+import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import Navbar from './components/Navbar';
-import UserList from './components/UserList';
-import Search from './components/Search';
+import Home from './components/Home';
 import Alert from './components/Alert'
 import About from './components/About';
 import UserInfo from './components/UserInfo';
 
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
+import NotFound from './components/NotFound';
 
 const App = () => {
   return (
@@ -19,20 +19,14 @@ const App = () => {
       <AlertState>
         <Router>
           <div className="App">
-            <Navbar icon='fab fa-github' title='Github App'/>
+            <Navbar icon='fab fa-github' title='Gitbook App'/>
               <div className="container">
                 <Alert />
                 <Switch>
-                  <Route exact path='/' render={props => (
-                    <Fragment>
-                      <Search />
-                      <UserList />
-                    </Fragment>
-                  )} />
+                  <Route exact path='/' component={Home}/>
                   <Route exact path='/about' component={About} />
-                  <Route exact path='/user/:login' render={props => (
-                    <UserInfo {...props} />
-                  )} />
+                  <Route exact path='/user/:login' component={UserInfo}/>
+                  <Route component={NotFound} />
                 </Switch> 
               </div>
           </div>
