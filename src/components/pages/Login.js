4785afa90df6d62768ccd2b94102885a169fde01
Shawn Rice <Shawn.CS@protonmail.com>
2022-07-02 12:15:22 -0400

    MINOR - Added message to inform user that login/registration are not functional

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -9,29 +9,34 @@ const Login = () => {
   const { darkMode } = themeContext;
 
   return (
-    <Formik
-      initialValues={{
-        username: "",
-        password: "",
-      }}
-      validationSchema={loginSchema}
-      //onSubmit={values => }
-    >
-      {(formik) => (
-        <div>
-          <h1>Login</h1>
-          <Form className='form'>
-            <FormField label='Username' name='username' type='text' />
-            <FormField label='Password' name='password' type='password' />
-            <input
-              type='submit'
-              value='Submit'
-              className={`btn btn-block ${darkMode ? "btn-light" : "btn-dark"}`}
-            />
-          </Form>
-        </div>
-      )}
-    </Formik>
+    <>
+      <p>Please note that these forms do not function yet.</p>
+      <Formik
+        initialValues={{
+          username: "",
+          password: "",
+        }}
+        validationSchema={loginSchema}
+        //onSubmit={values => }
+      >
+        {(formik) => (
+          <div>
+            <h1>Login</h1>
+            <Form className='form'>
+              <FormField label='Username' name='username' type='text' />
+              <FormField label='Password' name='password' type='password' />
+              <input
+                type='submit'
+                value='Submit'
+                className={`btn btn-block ${
+                  darkMode ? "btn-light" : "btn-dark"
+                }`}
+              />
+            </Form>
+          </div>
+        )}
+      </Formik>
+    </>
   );
 };
 
--- src/components/pages/Registration.js
+++ src/components/pages/Registration.js
@@ -9,37 +9,42 @@ const Registration = () => {
   const { darkMode } = themeContext;
 
   return (
-    <Formik
-      initialValues={{
-        email: "",
-        username: "",
-        password: "",
-        confirmPassword: "",
-      }}
-      validationSchema={registrationSchema}
-      //onSubmit={values => }
-    >
-      {(formik) => (
-        <div>
-          <h1>Register</h1>
-          <Form className='form'>
-            <FormField label='Email' name='email' type='email' />
-            <FormField label='Username' name='username' type='text' />
-            <FormField label='Password' name='password' type='password' />
-            <FormField
-              label='Confirm Password'
-              name='confirmPassword'
-              type='password'
-            />
-            <input
-              type='submit'
-              value='Submit'
-              className={`btn btn-block ${darkMode ? "btn-light" : "btn-dark"}`}
-            />
-          </Form>
-        </div>
-      )}
-    </Formik>
+    <>
+      <p>Please note that these forms do not function yet.</p>
+      <Formik
+        initialValues={{
+          email: "",
+          username: "",
+          password: "",
+          confirmPassword: "",
+        }}
+        validationSchema={registrationSchema}
+        //onSubmit={values => }
+      >
+        {(formik) => (
+          <div>
+            <h1>Register</h1>
+            <Form className='form'>
+              <FormField label='Email' name='email' type='email' />
+              <FormField label='Username' name='username' type='text' />
+              <FormField label='Password' name='password' type='password' />
+              <FormField
+                label='Confirm Password'
+                name='confirmPassword'
+                type='password'
+              />
+              <input
+                type='submit'
+                value='Submit'
+                className={`btn btn-block ${
+                  darkMode ? "btn-light" : "btn-dark"
+                }`}
+              />
+            </Form>
+          </div>
+        )}
+      </Formik>
+    </>
   );
 };
 
